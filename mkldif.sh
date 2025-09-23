a8f99fdeb24380d8aff1f31e7b10ac7ddc40c9e9
Petr Menšík <pemensik@redhat.com>
2025-09-23 17:51:48 +0200

    Update generating script to produce also dnsperf input

--- mkldif.sh
+++ mkldif.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 INPUT=${1:-DEFAULT/fedoraproject.org}
-: ${LDAP_SUFFIX:="dc=dns, dc=ipa, dc=test"}
+: ${LDAP_SUFFIX:="cn=dns, dc=ipa, dc=test"}
 
 ldap_name() {
 	echo $1 | sed -e 's/^/dc=/' -e 's/\./,dc=/g'
@@ -10,20 +10,39 @@ ldap_name() {
 make_ldif() {
 	local ZONEFILE="${1}"
 	local ZONE=${2:-$(basename "$ZONEFILE")}
-	local LDAP_BASE="$(ldap_name $ZONE), ${LDAP_SUFFIX}"
+#	local LDAP_BASE="$(ldap_name $ZONE), ${LDAP_SUFFIX}"
+	local LDAP_BASE="${LDAP_SUFFIX}"
 
+	echo "# $ZONE from $ZONEFILE"
 	python3 ./zone2dyndb-ldif.py $ZONEFILE $ZONE "${LDAP_BASE}" > "$ZONEFILE.ldif"
+}
+
+make_dnsperf() {
+	local ZONEFILE="${1}"
+	local ZONE=${2:-$(basename "$ZONEFILE")}
+	local TMPZONE="$(mktemp --tmpdir XXXXXXXX.zone)"
+	local PERFFILE="$ZONEFILE.dnsperf"
+
+	named-compilezone -s full -o "$TMPZONE" "$ZONE" "$ZONEFILE"
+	awk '{print $1, $4}' "$TMPZONE" > "$PERFFILE"
+	rm -f "$TMPZONE"
 }
 
 make_dir() {
 	local INPUT="$1"
-	for FILE in $(ls -1 "$INPUT"/* | grep -vE '\.(ldif|signed)'); do
-		[ -f "$FILE" ] && make_ldif "$FILE"
+	for FILE in $(ls -1 "$INPUT"/* | grep -vE '\.(ldif|signed|dnsperf)'); do
+		if [ -f "$FILE" ]; then
+			make_ldif "$FILE"
+			make_dnsperf "$FILE"
+		fi
 	done
 }
 
 if [ -d "$INPUT" ]; then
 	make_dir "$INPUT"
+	cat "$INPUT"/*.dnsperf > $INPUT.dnsperf
 else
-	make_ldif "$@"
+	[ $# -gt 1 ] && shift
+	make_ldif "$INPUT" "$@"
+	make_dnsperf "$FILE" "$@"
 fi
